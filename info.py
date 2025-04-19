f03ca4164a0453f7a836d9f06186a9adb1bb2d3c
AF <gulabsinghrajput008@gmail.com>
2025-04-19 21:13:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,8 +127,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'd72a21ce0583
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'd72a21ce05839e8af9519a05976bfe6982299e73')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shrinkearn.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '990234585c542de2df82a2270f3beb82cc8e49a8')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/newmoviesall10/6') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
